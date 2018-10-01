cd60be4980dc4a4aa31ba753cbcda57585ea930c
Asif Junaid <asif.junaid@pwc.com>
2018-10-01 15:35:56 +0400

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Asif Junaid on 10/1/18.
+//  Copyright © 2018 Asif Junaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
